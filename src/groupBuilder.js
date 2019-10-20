f4345787bcd2064186367ecf170f3532a2f7458f
Abhishek Das <abhi.das2007das@gmail.com>
2019-10-20 08:17:03 +0100

    fix(groupBuilder): eslint issues and added jsDoc comments for the functions

--- src/groupBuilder.js
+++ src/groupBuilder.js
@@ -1,83 +1,8 @@
-const data = [
-  {
-    pickle: {
-      tags: [
-        {
-          name: '@groupB',
-          location: {
-            line: 1,
-            column: 1,
-          },
-        },
-        {
-          name: '@SW_ABC_ON',
-          location: {
-            line: 1,
-            column: 9,
-          },
-        },
-        {
-          name: '@SW_ABC_ON',
-          location: {
-            line: 13,
-            column: 3,
-          },
-        },
-      ],
-      name: 'First scenario related to groupB',
-      language: 'en',
-      locations: [Array],
-      steps: [Array],
-    },
-    uri: 'features/groupB.feature',
-  },
-  {
-    pickle: {
-      tags: [
-        {
-          name: '@SW_ABC_ON',
-          location: {
-            line: 1,
-            column: 1,
-          },
-        },
-        {
-          name: '@SW_ABC_ON',
-          location: {
-            line: 1,
-            column: 9,
-          },
-        },
-        {
-          name: '@firstB',
-          location: {
-            line: 13,
-            column: 3,
-          },
-        },
-      ],
-      name: 'Second scenario related to groupB',
-      language: 'en',
-      locations: [Array],
-      steps: [Array],
-    },
-    uri: 'features/groupB.feature',
-  },
-  {
-    pickle: {
-      tags: [],
-      name: 'Second scenario related to groupB',
-      language: 'en',
-      locations: [Array],
-      steps: [Array],
-    },
-    uri: 'features/groupB.feature',
-  },
-];
-const data1 = /SW_/;
-
-
-// extract all tags from scenario
+/**
+ * @description Extract all the tags from the valid scenarios
+ * @param {Object} scenarios Object returned by getValidTestCases
+ * @returns Array of array of all tags for all the valid scenarios
+ */
 const extractAllTags = (scenarios) => {
   try {
     return scenarios.map((scenario) => {
@@ -91,13 +16,22 @@ const extractAllTags = (scenarios) => {
 };
 
 
+/**
+ * @description Build unique groups based on the grouping tags
+ * @param {Array} scenariosTags 2D array returned by extractAllTags function
+ * @param {RegularExpression} tagForGrouping Regular expression that can identify grouping tags
+ * @return Array of unique groups based on grouping tags
+ */
 const buildUniqueGroups = (scenariosTags, tagForGrouping) => {
-  // array of array of all the group-tags for each scenario
+  // step-1: array of array of all the group-tags for each scenario
   const scenariosGroupTags = scenariosTags.map(
     (scenarioTags) => scenarioTags.filter((scenarioTag) => tagForGrouping.test(scenarioTag)),
   );
 
+  // step-2: array of array of all unique group-tags for each scenario
   const scenariosUniqueTags = scenariosGroupTags.map((scenarioGroupTags) => [...new Set(scenarioGroupTags)]);
+
+  // step-3: array of unique groups of grouping tags
   return [...new Set(scenariosUniqueTags.map((scenarioUniqueTags) => scenarioUniqueTags.join()))];
 };
 
